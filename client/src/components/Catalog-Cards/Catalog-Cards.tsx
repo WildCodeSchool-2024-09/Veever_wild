0c0710f8e85f8a833f7a9ccff377761970d881da
nicochnz <chiche.nicolas@gmail.com>
2025-01-08 16:18:49 +0100

    add chakra component for the card

--- client/src/components/Catalog-Cards/Catalog-Cards.tsx
+++ client/src/components/Catalog-Cards/Catalog-Cards.tsx
@@ -1,13 +1,24 @@
+import { system } from "@chakra-ui/react/preset";
+import "./Catalog-Cards.css";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import ChrBottomCard from "./Chr-Bottom-Card";
 import ChrMiddleCard from "./Chr-Middle-Card";
 import ChrTopCard from "./Chr-Top-Card";
 
 export default function CatalogCards() {
   return (
-    <>
+    <div className="chr-full-card">
       <ChrTopCard />
-      <ChrMiddleCard />
-      <ChrBottomCard />
-    </>
+      <ChakraProvider value={system}>
+        <Box maxW="sm" borderWidth="1px" overflow="hidden">
+          <ChrMiddleCard />
+          <Box p="6">
+            <Box className="bottomCard" mt="4" display="flex" gap="4">
+              <ChrBottomCard />
+            </Box>
+          </Box>
+        </Box>
+      </ChakraProvider>
+    </div>
   );
 }
