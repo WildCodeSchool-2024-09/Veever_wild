4b6113e72edb212164844b1eef4b36acab785a02
Sarah Smandack <sarah.smandack@gmail.com>
2025-01-09 10:44:20 +0100

    export SignUp button in a component

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,28 +1,17 @@
-import { useNavigate } from "react-router-dom";
 import ExternalSystemLogin from "../../components/Login/ExternalSystemLogin";
 import LoginForm from "../../components/Login/LoginForm";
 import PasswordForgotten from "../../components/Login/PasswordForgotten";
+import SignUp from "../../components/Login/SignUp";
 import "./Login.css";
 
 export default function Login() {
-  const navigate = useNavigate();
   return (
     <>
       <LoginForm />
       <PasswordForgotten />
       <p>ou</p>
       <ExternalSystemLogin />
-
-      {/* TODO : Modifier par le composant d'inscription quand merge sur dev */}
-      <button
-        type="button"
-        className="subscribeBtn"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        S'inscrire
-      </button>
+      <SignUp />
     </>
   );
 }
