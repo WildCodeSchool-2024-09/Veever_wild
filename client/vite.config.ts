d2c3820be9d6ee6716ee0affc03ad1b671804889
Raphaël Streiff <raphaelstreiff93@gmail.com>
2025-01-11 18:59:11 +0100

    Initiate Clients_Modules and fix biome request

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
-    }
+    },
   },
   server: {
     port: 3000,
--- server/src/router.ts
+++ server/src/router.ts
@@ -20,6 +20,12 @@ router.put("/api/hotels/:id", hotelsActions.edit);
 router.post("/api/hotels", hotelsActions.add);
 router.delete("/api/hotels/:id", hotelsActions.destroy);
 
+import clientsActions from "./modules/clients/clientsActions";
+router.get("/api/clients", clientsActions.browse);
+router.get("/api/clients/:id", clientsActions.read);
+router.post("/api/clients", clientsActions.add);
+router.put("/api/clients/:id", clientsActions.update);
+router.delete("/api/clients/:id", clientsActions.destroy);
 /* ************************************************************************* */
 
 export default router;
