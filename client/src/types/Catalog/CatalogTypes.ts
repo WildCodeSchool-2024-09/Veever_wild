bbd3c72a874117fd2c70f9fab264d612aba597e3
nicochnz <chiche.nicolas@gmail.com>
2025-01-15 09:25:56 +0100

    fix types

--- client/src/types/Catalog/CatalogTypes.ts
+++ client/src/types/Catalog/CatalogTypes.ts
@@ -22,3 +22,21 @@ export type CatalogBoxesProps = {
 export type CatalogRecapProps = {
   data: RecapItem[];
 };
+export type Card = {
+  id: number;
+  picture: string;
+  name: string;
+  maxPrice: number;
+  address: string;
+};
+export type Props = {
+  children: React.ReactNode;
+};
+
+export type SaveCardsContextType = {
+  saveCards: Card[];
+  setSaveCards: React.Dispatch<React.SetStateAction<Card[]>>;
+  addCard: (card: Card) => void;
+  removeCard: (cardId: number) => void;
+  isCardSaved: (cardId: number) => boolean;
+};
