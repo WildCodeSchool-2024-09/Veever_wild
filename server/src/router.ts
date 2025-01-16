d851f73e7997348b127ec2273bbd28405ae1585f
Sarah Smandack <sarah.smandack@gmail.com>
2025-01-16 15:29:41 +0100

    create module user

--- server/src/router.ts
+++ server/src/router.ts
@@ -28,34 +28,8 @@ router.put("/api/hotels/:id", hotelsActions.edit);
 router.post("/api/hotels", hotelsActions.add);
 router.delete("/api/hotels/:id", hotelsActions.destroy);
 
-router.post("/api/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const [result] = await databaseClient.query<Rows>(
-      `
-    SELECT email, password
-    FROM user
-    WHERE email = ?
-    `,
-      [email],
-    );
-
-    if (result.length === 0) {
-      res.status(404).json({ message: "Email non trouvé" });
-    }
-
-    const user = result[0];
-
-    if (user.password !== password) {
-      res.sendStatus(401).json({ message: "Mot de passe incorrect" });
-    }
-
-    res.status(200).json({ message: "Connexion réussie" });
-  } catch (error) {
-    res.sendStatus(500);
-  }
-});
+import userActions from "./modules/user/userActions";
+router.post("/api/login", userActions.authenticateUser);
 
 /* ************************************************************************* */
 
