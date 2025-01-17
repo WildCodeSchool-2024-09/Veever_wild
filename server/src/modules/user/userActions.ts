789be6d2d9e4c1e99aa9e7c713aeb014326fa593
Sarah Smandack <sarah.smandack@gmail.com>
2025-01-17 14:02:41 +0100

    update Actions & Repository: add token jwt

--- server/src/modules/user/userActions.ts
+++ server/src/modules/user/userActions.ts
@@ -1,11 +1,12 @@
 import bcrypt from "bcryptjs";
 import type { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
 import userRepository from "./userRepository";
 
 const authenticateUser: RequestHandler = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await userRepository.getUserEmail(email);
+    const user = await userRepository.getUserEmailWithRole(email);
 
     if (!user) {
       res.sendStatus(404);
@@ -17,7 +18,16 @@ const authenticateUser: RequestHandler = async (req, res, next) => {
       res.sendStatus(401);
     }
 
-    res.sendStatus(200);
+    const token = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" },
+    );
+
+    res.status(200).json({
+      message: "Connexion réussie",
+      token,
+    });
   } catch (error) {
     next(error);
   }
