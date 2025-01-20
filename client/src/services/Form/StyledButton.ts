902c29c30e5eb87ec15a54108322a8e1cbcc7270
Nino Jautee <jauteenino@gmail.com>
2025-01-20 21:33:06 +0100

    add definitive font

---
+++ client/src/services/Form/StyledButton.ts
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const StyledButton = styled(Button)(
+  `
+  background: var(--bckgColor-CTA);
+  background: var(--gradientColor-CTA);
+  `,
+);
