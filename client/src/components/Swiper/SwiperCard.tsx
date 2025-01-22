ae8a819ef36a1463cb510631acb7cd30e9266d57
Nino Jautee <jauteenino@gmail.com>
2025-01-22 21:06:15 +0100

    styling swipper

--- client/src/components/Swiper/SwiperCard.tsx
+++ client/src/components/Swiper/SwiperCard.tsx
@@ -24,9 +24,13 @@ export default function Cardz({
     x,
   } = useSwiper(cards, setCards, id, handleKeywordSelection);
   return (
-    <article className="swiper-container" id={`swiper-${id.toString()}`}>
-      <article
-        className={`swiper-card swiper-card-${cards[currentIndex].type}`}
+    <main className="swiper-container">
+      <motion.article
+        className="swiper-card"
+        dragConstraints={{ left: 0, right: 0 }}
+        drag="x"
+        style={{ x, opacity, rotate }}
+        onDragEnd={() => handleDragEnd(id)}
       >
         {isSnackOpen && (
           <Snackbar
@@ -37,16 +41,16 @@ export default function Cardz({
             transitionDuration={1000}
           />
         )}
-        <motion.img
-          className="img-swiper"
-          src={picture}
-          alt={name}
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }}
-          style={{ x, opacity, rotate }}
-          onDragEnd={() => handleDragEnd(id)}
-        />
-
+        <p>{cards[currentIndex].name}</p>
+        <article className="tag-img-container">
+          <img className="img-swiper" src={picture} alt={name} />
+          <aside className="tag-container">
+            <p>Type: {cards[currentIndex].category}</p>
+            <p>{cards[currentIndex].address}</p>
+            <p>Prix entre {cards[currentIndex].minPrice}$</p>
+            <p>et {cards[currentIndex].maxPrice}$</p>
+          </aside>
+        </article>
         <aside className="button-container">
           <button
             className="swiper-btn"
@@ -54,28 +58,26 @@ export default function Cardz({
             type="button"
           >
             <svg
-              viewBox="0 0 16 16"
               xmlns="http://www.w3.org/2000/svg"
+              fill="#fb7a3f"
               width="60"
               height="60"
+              viewBox="0 0 612.043 612.043"
             >
-              <g transform="translate(-52.11 -582.91)">
-                <title>J'aime pas</title>
-                <path
-                  d="m64.792 582.91-4.6811 4.6811-4.6811-4.6811-3.3189 3.3189 4.6811 4.6811-4.6811 4.6811 3.3189 3.3189 4.6811-4.6811 4.6811 4.6811 3.3189-3.3189-4.6811-4.6811 4.6811-4.6811-3.3189-3.3189z"
-                  fill="#ef3416"
-                  stroke="#242124"
-                  strokeWidth="0.3"
-                />
+              <title>J'aime pas</title>
+              <g>
+                <g id="cross">
+                  <g>
+                    <path
+                      d="M397.503,306.011l195.577-195.577c25.27-25.269,25.27-66.213,0-91.482c-25.269-25.269-66.213-25.269-91.481,0
+				L306.022,214.551L110.445,18.974c-25.269-25.269-66.213-25.269-91.482,0s-25.269,66.213,0,91.482L214.54,306.033L18.963,501.61
+				c-25.269,25.269-25.269,66.213,0,91.481c25.269,25.27,66.213,25.27,91.482,0l195.577-195.576l195.577,195.576
+				c25.269,25.27,66.213,25.27,91.481,0c25.27-25.269,25.27-66.213,0-91.481L397.503,306.011z"
+                    />
+                  </g>
+                </g>
               </g>
             </svg>
-          </button>
-          <button
-            className="swiper-btn"
-            type="button"
-            aria-labelledby={cards[currentIndex].type}
-          >
-            <Link to="/prestataire">+ d'infos</Link>
           </button>
           <button
             className="swiper-btn"
@@ -84,24 +86,28 @@ export default function Cardz({
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="85"
-              height="85"
-              id="heart"
-              viewBox="0 0 16 16"
+              width="70"
+              height="80"
+              fill="#7BB471"
+              viewBox="0 0 426.668 426.668"
             >
-              <title>Jj'aime</title>
+              <title>J'aime</title>
               <path
-                fill="#ef3416"
-                stroke="#242124"
-                strokeWidth="0.3"
-                d="M5.301 3.002c-.889-.047-1.759.247-2.404.893-1.29 1.292-1.175 3.49.26 4.926l.515.515L8.332 14l4.659-4.664.515-.515c1.435-1.437 1.55-3.634.26-4.926-1.29-1.292-3.483-1.175-4.918.262l-.516.517-.517-.517C7.098 3.438 6.19 3.049 5.3 3.002z"
-              >
-                J'aime
-              </path>
+                d="M401.788,74.476c-63.492-82.432-188.446-33.792-188.446,49.92
+	c0-83.712-124.962-132.356-188.463-49.92c-65.63,85.222-0.943,234.509,188.459,320.265
+	C402.731,308.985,467.418,159.698,401.788,74.476z"
+              />
             </svg>
           </button>
+          <button
+            className="swiper-btn"
+            type="button"
+            aria-labelledby={cards[currentIndex].type}
+          >
+            <Link to="/prestataire">VOIR PLUS D'INFOS +</Link>
+          </button>
         </aside>
-      </article>
-    </article>
+      </motion.article>
+    </main>
   );
 }
