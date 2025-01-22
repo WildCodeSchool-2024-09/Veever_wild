3043ae7f1181296593f300a9a2fc583725317716
Nino Jautee <jauteenino@gmail.com>
2025-01-22 18:22:41 +0100

    add some style

---
+++ client/src/components/Swiper/Swiperheader.tsx
@@ -0,0 +1,13 @@
+export default function SwiperHeader() {
+  return (
+    <>
+      <aside className="swiper-header">
+        <img
+          className="logo-swiper"
+          src="./src/assets/images/LogoVeever.png"
+          alt=""
+        />
+      </aside>
+    </>
+  );
+}
