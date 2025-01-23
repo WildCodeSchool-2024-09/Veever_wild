5161eb4621eb1328776a9c3e0e182b694fca90a3
Sarah Smandack <sarah.smandack@gmail.com>
2025-01-23 17:22:01 +0100

    fix date in CatalogRecap

--- client/src/components/Catalog/Catalog-Recap/CatalogRecap.tsx
+++ client/src/components/Catalog/Catalog-Recap/CatalogRecap.tsx
@@ -2,6 +2,8 @@ import type { CatalogRecapProps } from "../../../types/Catalog/CatalogTypes";
 import "./CatalogRecap.css";
 
 export default function CatalogRecap({ data }: CatalogRecapProps) {
+  const date = new Date();
+
   return (
     <>
       <ul className="catalog-filter-section">
@@ -15,7 +17,14 @@ export default function CatalogRecap({ data }: CatalogRecapProps) {
           </li>
         ))}
       </ul>
-      <h2 className="date-card">Date : 20-11-1994</h2>
+      <h2 className="date-card">
+        Date :{" "}
+        {date.toLocaleDateString("fr-FR", {
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+        })}
+      </h2>
       <button className="button-throw-likes-away" type="button">
         Vider les likes de ce jour
       </button>
