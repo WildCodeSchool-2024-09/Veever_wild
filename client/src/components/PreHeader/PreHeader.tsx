2bcc6d7917bc655956d070b42ca5feb101c48c8f
H4lfbeard <humbert.thomas.pro@gmail.com>
2025-01-27 15:39:59 +0100

    header

--- client/src/components/PreHeader/PreHeader.tsx
+++ client/src/components/PreHeader/PreHeader.tsx
@@ -2,11 +2,13 @@ import "./PreHeader.css";
 export default function PreHeader() {
   return (
     <section className="preheader-section">
+      <div className="logobox">
+        <img className="logo" src="public/assets/images/logo.png" alt="logo" />
+      </div>
       <select name="city" className="city-select">
         <option value="">Choisir une ville</option>
         <option value="bordeaux">❤️ Bordeaux</option>
       </select>
-      <img className="logo" src="src/assets/images/logo.svg" alt="logo" />
     </section>
   );
 }
