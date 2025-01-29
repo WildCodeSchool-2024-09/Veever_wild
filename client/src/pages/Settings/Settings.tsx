c5058066d88718b91f0951d5f12003dd9f92883d
Raphaël Streiff <raphaelstreiff93@gmail.com>
2025-01-29 15:00:57 +0100

    delete path RestaurantTimeSlot on settings page (display test)

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,11 +1,7 @@
-import RestaurantPage from "../RestaurantBooking/RestaurantBooking";
-// test on a page settings to see what it look likes
-
 export default function Settings() {
   return (
     <>
-      {/* <h2>Settings</h2> */}
-      <RestaurantPage />
+      <h2>Settings</h2>
     </>
   );
 }
