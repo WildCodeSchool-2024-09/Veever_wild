d48a7db852e2694be3256020aa07878b8857a735
Nino Jautee <jauteenino@gmail.com>
2025-02-05 11:14:04 +0100

    add loader to swiper

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,6 +11,7 @@ import Swiper from "./components/Swiper/Swiper";
 import Catalog from "./pages/Catalog/Catalog";
 import HomePage from "./pages/HomePage/HomePage";
 import Settings from "./pages/Settings/Settings";
+import { chrLoader } from "./services/Loader/LoaderChr";
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
 
@@ -36,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "/search",
         element: <Swiper />,
+        loader: chrLoader,
       },
       {
         path: "/settings",
